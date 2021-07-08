30c06b717c6b28eeb3b0604a72fc089e8257e096
HayatoTanoue <hayatotanoue7321@gmail.com>
2021-07-08 14:02:45 +0900

    autp pypi test upload

--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
 from setuptools import setup, find_packages
 
 import reserch_utils_HT
-requires = ["matplotlib","networkx", "numpy"]
+
+requires = ["matplotlib", "networkx", "numpy", "pillow"]
 
 
 setup(
-    name='reserch_utils_HT',
+    name="reserch_utils_HT",
     version=reserch_utils_HT.__version__,
-    description='complex network model, graph to image utils',
-    url='https://github.com/HayatoTanoue/reserch_utils_HT',
+    description="complex network model, graph to image utils",
+    url="https://github.com/HayatoTanoue/reserch_utils_HT",
     author=reserch_utils_HT.__author__,
-    author_email='hayatotanoue7321@gmail.com',
+    author_email="hayatotanoue7321@gmail.com",
     license=reserch_utils_HT.__license__,
-    keywords='complex networks',
+    keywords="complex networks",
     packages=find_packages(),
     install_requires=requires,
     classifiers=[
-        'Programming Language :: Python :: 3',
+        "Programming Language :: Python :: 3",
     ],
 )
