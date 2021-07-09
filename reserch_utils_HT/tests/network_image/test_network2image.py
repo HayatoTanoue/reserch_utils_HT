6844849ac5307809332ab94b1a827b2d0cbd50d3
HayatoTanoue <hayatotanoue7321@gmail.com>
2021-07-09 12:37:00 +0900

    image2net test

--- reserch_utils_HT/tests/network_image/test_network2image.py
+++ reserch_utils_HT/tests/network_image/test_network2image.py
@@ -7,3 +7,25 @@ def test_network_to_image():
     G = nx.barabasi_albert_graph(10, 2)
     assert network_to_image(G, sort=False)
     assert network_to_image(G, sort=True)
+
+
+def test_image_to_network():
+    image_dir = "./reserch_utils_HT/tests/network_image/images/"
+
+    origin = nx.barabasi_albert_graph(100, 2, seed=10)
+    G = image_to_network(image_dir + "BA_m2_seed10.png")
+    G_sort = image_to_network(image_dir + "BA_sort_m2_seed10.png")
+
+    assert nx.number_of_edges(G) == nx.number_of_edges(origin)
+    assert nx.number_of_edges(G_sort) == nx.number_of_edges(origin)
+
+    assert nx.is_isomorphic(G, origin)
+    assert nx.is_isomorphic(G, G_sort)
+    assert nx.is_isomorphic(origin, G_sort)
+
+    origin = nx.barabasi_albert_graph(1000, 2, seed=1)
+    G = image_to_network(image_dir + "BA1000_m2_seed1.png")
+    G_sort = image_to_network(image_dir + "BA1000_sort_m2_seed1.png")
+
+    assert nx.number_of_edges(G) == nx.number_of_edges(origin)
+    assert nx.number_of_edges(G_sort) == nx.number_of_edges(origin)
