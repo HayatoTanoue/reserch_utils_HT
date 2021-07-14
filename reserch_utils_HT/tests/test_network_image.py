2c02d9c8b5fb5f2b079563631b8960ee6eddd1d8
HayatoTanoue <hayatotanoue7321@gmail.com>
2021-07-14 14:33:05 +0900

    del save img

--- reserch_utils_HT/tests/test_network_image.py
+++ reserch_utils_HT/tests/test_network_image.py
@@ -14,9 +14,9 @@ def test_image_to_network():
 
     origin = nx.barabasi_albert_graph(100, 2, seed=10)
     img = network_to_image(origin)
-    img.save(image_dir + "BA_m2_seed10.png")
+    # img.save(image_dir + "BA_m2_seed10.png")
     img = network_to_image(origin, sort=True)
-    img.save(image_dir + "BA_sort_m2_seed10.png")
+    # img.save(image_dir + "BA_sort_m2_seed10.png")
 
     G = image_to_network(image_dir + "BA_m2_seed10.png")
     G_sort = image_to_network(image_dir + "BA_sort_m2_seed10.png")
@@ -30,9 +30,9 @@ def test_image_to_network():
 
     origin = nx.barabasi_albert_graph(1000, 2, seed=1)
     img = network_to_image(origin)
-    img.save(image_dir + "BA1000_m2_seed1.png")
+    # img.save(image_dir + "BA1000_m2_seed1.png")
     img = network_to_image(origin, sort=True)
-    img.save(image_dir + "BA1000_sort_m2_seed1.png")
+    # img.save(image_dir + "BA1000_sort_m2_seed1.png")
 
     G = image_to_network(image_dir + "BA1000_m2_seed1.png")
     G_sort = image_to_network(image_dir + "BA1000_sort_m2_seed1.png")
