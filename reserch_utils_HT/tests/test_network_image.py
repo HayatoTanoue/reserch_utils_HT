9067febe18697d44bdc2728544b43e79fc62f425
HayatoTanoue <hayatotanoue7321@gmail.com>
2021-07-14 14:58:10 +0900

    test img unlink

--- reserch_utils_HT/tests/test_network_image.py
+++ reserch_utils_HT/tests/test_network_image.py
@@ -1,3 +1,4 @@
+import os
 import networkx as nx
 
 from reserch_utils_HT.src.network_image import *
@@ -10,16 +11,15 @@ def test_network_to_image():
 
 
 def test_image_to_network():
-    image_dir = "./reserch_utils_HT/tests/images/"
-
     origin = nx.barabasi_albert_graph(100, 2, seed=10)
     img = network_to_image(origin)
-    # img.save(image_dir + "BA_m2_seed10.png")
-    img = network_to_image(origin, sort=True)
-    # img.save(image_dir + "BA_sort_m2_seed10.png")
+    img.save("BA_m2_seed10.png")
 
-    G = image_to_network(image_dir + "BA_m2_seed10.png")
-    G_sort = image_to_network(image_dir + "BA_sort_m2_seed10.png")
+    img = network_to_image(origin, sort=True)
+    img.save("BA_sort_m2_seed10.png")
+
+    G = image_to_network("BA_m2_seed10.png")
+    G_sort = image_to_network("BA_sort_m2_seed10.png")
 
     assert nx.number_of_edges(G) == nx.number_of_edges(origin)
     assert nx.number_of_edges(G_sort) == nx.number_of_edges(origin)
@@ -28,14 +28,20 @@ def test_image_to_network():
     assert nx.is_isomorphic(G, G_sort)
     assert nx.is_isomorphic(origin, G_sort)
 
+    os.unlink("BA_m2_seed10.png")
+    os.unlink("BA_sort_m2_seed10.png")
+
     origin = nx.barabasi_albert_graph(1000, 2, seed=1)
     img = network_to_image(origin)
-    # img.save(image_dir + "BA1000_m2_seed1.png")
+    img.save("BA1000_m2_seed1.png")
     img = network_to_image(origin, sort=True)
-    # img.save(image_dir + "BA1000_sort_m2_seed1.png")
+    img.save("BA1000_sort_m2_seed1.png")
 
-    G = image_to_network(image_dir + "BA1000_m2_seed1.png")
-    G_sort = image_to_network(image_dir + "BA1000_sort_m2_seed1.png")
+    G = image_to_network("BA1000_m2_seed1.png")
+    G_sort = image_to_network("BA1000_sort_m2_seed1.png")
 
     assert nx.number_of_edges(G) == nx.number_of_edges(origin)
     assert nx.number_of_edges(G_sort) == nx.number_of_edges(origin)
+
+    os.unlink("BA1000_m2_seed1.png")
+    os.unlink("BA1000_sort_m2_seed1.png")
